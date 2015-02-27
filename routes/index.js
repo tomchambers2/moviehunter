e24f09525800576048b216f37907ba670f848f6a
Tom Chambers <tom.chambers@gmail.com>
2015-02-27 17:41:15 +0000

    added routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/Sitemap.xml', function(req, res) {
+	res.set('Content-Type', 'text/xml');
+	res.sendfile('Sitemap.xml');
+});
+
+router.get('*', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
