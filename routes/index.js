0b004a9d6f358422de405c8271eae4411106605e
Tom Chambers <tom.chambers@gmail.com>
2015-03-11 20:27:43 +0000

    add sitemap absolute path

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/Sitemap.xml', function(req, res) {
+router.get('/app/public/Sitemap.xml', function(req, res) {
 	res.set('Content-Type', 'text/xml');
 	res.sendfile('Sitemap.xml');
 });
